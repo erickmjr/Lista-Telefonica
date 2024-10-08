bdb3d0ea9a0a58bb43d6c78fa401c526da3cf7bb
erickmjr <erickmellojr@gmail.com>
2024-10-07 21:39:51 -0300

    Adicionando ação de deletar contato + adicionando ícone à página

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ const msgErro = document.getElementById("msgErro");
 const arrayNomes = [];
 const arrayTelefones = [];
 
+let i = 0;
+
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
@@ -36,6 +39,7 @@ function verificarStringTel(){
 
 
 function adicionarContato(){
+
     if(arrayNomes.includes(nome.value.trim())){
 
         msgErro.textContent = (`Já existe um contato com o nome "${nome.value}" na lista.`);
@@ -58,11 +62,40 @@ function adicionarContato(){
 function adicionarLinha(){
     telefone.value = "(" + telefone.value.slice(0,2) + ")" + " " + telefone.value.slice(2,7) + "-" + telefone.value.slice(7);
     
-    tbodyTable.innerHTML += `<tr><td>${nome.value}</td> <td>${telefone.value}</td></tr>`
+    let i = tbodyTable.rows.length;
+
+    tbodyTable.innerHTML += `<tr id = "index${i}">
+    <td>${nome.value}</td> 
+    <td>${telefone.value}</td>
+    <td>
+    <button class = "btn-apagar" onclick="excluirContato(${i})" type="submit">🗑️</button></td>
+    </tr>`
 
     removerMensagem();
+
 }
 
 function removerMensagem(){
-    msgContatos.style.display = "none";
+
+    if (arrayNomes.length  > 0){
+        msgContatos.style.display = "none";
+    } else{
+        msgContatos.style.display = "block";
+    }
+    
 }
+
+
+function excluirContato(i){
+    let linha = document.getElementById(`index${i}`);
+    linha.remove();
+
+    arrayNomes.splice(i, 1);
+    arrayTelefones.splice(i, 1);
+
+    removerMensagem();
+
+    console.log(arrayNomes);
+    console.log(arrayTelefones);
+}
+
